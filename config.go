4c81d800bcaf7ede9da3f65d0c93912943b6dfd1
bensooraj <admin@bensooraj.com>
2023-10-30 12:30:34 +0530

    block `ssh_key` with label

--- config.go
+++ config.go
@@ -1,10 +1,16 @@
 package main
 
 type Config struct {
-	Griffon GriffonBlock `hcl:"griffon,block"`
+	Griffon GriffonBlock  `hcl:"griffon,block"`
+	SSHKeys []SSHKeyBlock `hcl:"ssh_key,block"`
 }
 
 type GriffonBlock struct {
 	Region      string `hcl:"region,attr"`
 	VultrAPIKey string `hcl:"vultr_api_key"`
+}
+
+type SSHKeyBlock struct {
+	Name   string `hcl:"name,label"`
+	SSHKey string `hcl:"ssh_key"`
 }
--- parser_test.go
+++ parser_test.go
@@ -20,3 +20,31 @@ func TestParseGriffonBlock(t *testing.T) {
 		VultrAPIKey: "1234567890",
 	}, config.Griffon, "GriffonBlock parsed incorrectly")
 }
+
+func TestParseSshKeyBlock(t *testing.T) {
+	src := []byte(`
+	griffon {
+		region = "nyc1"
+		vultr_api_key = "1234567890"
+	}
+	
+	ssh_key "my_key" {
+		ssh_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADA"
+	}`)
+
+	config, err := ParseHCL("test.hcl", src)
+	require.NoError(t, err)
+	require.Equalf(t, GriffonBlock{
+		Region:      "nyc1",
+		VultrAPIKey: "1234567890",
+	}, config.Griffon, "GriffonBlock parsed incorrectly")
+
+	require.NotNil(t, config.SSHKeys, "SSHKeys not parsed")
+
+	require.Lenf(t, config.SSHKeys, 1, "len(SSHKeys); got %d, want 1", len(config.SSHKeys))
+
+	require.Equalf(t, SSHKeyBlock{
+		Name:   "my_key",
+		SSHKey: "ssh-rsa AAAAB3NzaC1yc2EAAAADA",
+	}, config.SSHKeys[0], "SSHKeyBlock parsed incorrectly")
+}
