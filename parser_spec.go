22b6a9f0f935112b3a97a3bb34647c0d9f525b0a
bensooraj <bensooraj@users.noreply.github.com>
2023-11-09 17:23:44 +0530

    feat(parser): parse HCL from `hcldec.Spec` and added corresponding tests

---
+++ parser_spec.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/hashicorp/hcl/v2"
+	"github.com/hashicorp/hcl/v2/hcldec"
+	"github.com/hashicorp/hcl/v2/hclparse"
+)
+
+func ParseHCLUsingSpec(filename string, src []byte, ctx *hcl.EvalContext) (*Config, error) {
+	var config Config
+	var diags hcl.Diagnostics
+
+	parser := hclparse.NewParser()
+	// Parse the HCL file
+	file, diags := parser.ParseHCL(src, filename)
+	if diags.HasErrors() {
+		return nil, diags
+	}
+
+	val, diags := hcldec.Decode(file.Body, ConfigSpec, ctx)
+	if diags.HasErrors() {
+		return nil, diags
+	}
+
+	for vName, v := range val.AsValueMap() {
+		switch vName {
+		case "griffon":
+			config.Griffon = GriffonBlock{
+				Region:      v.GetAttr("region").AsString(),
+				VultrAPIKey: v.GetAttr("vultr_api_key").AsString(),
+			}
+		case "ssh_key":
+			sshKeys := v.AsValueSlice()
+			for _, sshKey := range sshKeys {
+				config.SSHKeys = append(config.SSHKeys, SSHKeyBlock{
+					Name:   sshKey.GetAttr("name").AsString(),
+					SSHKey: sshKey.GetAttr("ssh_key").AsString(),
+				})
+			}
+		default:
+			return nil, fmt.Errorf("unknown block type %q", vName)
+		}
+	}
+
+	return &config, nil
+}
