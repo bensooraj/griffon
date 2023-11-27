1ebcc84ffb74f0ec38fd6a7477f81fb246600dfb
bensooraj <bensooraj@users.noreply.github.com>
2023-11-27 14:14:17 +0530

    some heavy restructuring, the code is broken at this point. This will happen again.

--- config.go
+++ config.go
@@ -1,27 +1,9 @@
 package main
 
-import (
-	"github.com/hashicorp/hcl/v2"
-	"github.com/vultr/govultr/v3"
-)
+import "github.com/bensooraj/griffon/blocks"
 
 type Config struct {
-	Griffon        GriffonBlock                  `hcl:"griffon,block"`
-	SSHKeys        map[string]SSHKeyBlock        `hcl:"ssh_key,block"`
-	StartupScripts map[string]StartupScriptBlock `hcl:"startup_script,block"`
-	Instances      map[string]InstanceBlock      `hcl:"instance,block"`
-	DataBlocks     map[string]map[string]Block   `hcl:"data,block"`
-}
-
-type Block interface {
-	ID() int64
-	// Separate the block into its configuration and dependencies
-	PreProcessHCLBlock(block *hcl.Block, ctx *hcl.EvalContext) error
-	// Process the configuration
-	ProcessConfiguration(ctx *hcl.EvalContext) error
-	// Get Dependencies
-	Dependencies() []string
-	// Execute the block by making API calls
-	// Execute(ctx *hcl.EvalContext) error
-	Create(ctx *hcl.EvalContext, vc *govultr.Client) error
+	Griffon   blocks.GriffonBlock             `hcl:"griffon,block" json:"griffon"`
+	Data      map[string]blocks.DataBlock     `hcl:"data,block" json:"data"`
+	Resources map[string]blocks.ResourceBlock `hcl:"resources,block" json:"resources"`
 }
